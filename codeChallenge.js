37aba45c8d551c1bf0076f27bc25c49cbcd03396
Selena Carlson <selena.h.carlson@gmail.com>
2018-02-05 14:40:26 -0600

    woohoo! she's working

--- codeChallenge.js
+++ codeChallenge.js
@@ -1,8 +1,22 @@
 $( document ).ready(function(){
+  var count = 0;
   $( '#generateButton' ).on('click', function(){
     console.log('in generateButton');
-    $( 'body' ).append( '<div></div>' );
-  })
+    count ++
+    $( 'body' ).append( "<div class='addedDiv'> <p>" + count + " <button id=swapButton>Swap</button> <button id=deleteButton>Delete</button> </p> </div>" );
 
+  }) // end generateButton
+
+$( 'body' ).on('click', '#swapButton', function(){
+    console.log('in swapButton');
+    $( this ).parent().toggleClass( 'swap' );
 
 })
+
+$( 'body' ).on('click', '#deleteButton', function(){
+    console.log('in deleteButton');
+    $( this ).parent().remove();
+})
+
+
+}) // end doc ready
